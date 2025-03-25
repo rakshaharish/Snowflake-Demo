76ee9f34d2034d2de7944e522ffcbad18618aada
Raksha Harish <raksha.techna@gmail.com>
2025-03-25 12:55:28 -0700

    Update 7. Time-travel Scenario.sql

--- 2. SQLs/7. Time-travel Scenario.sql
+++ 2. SQLs/7. Time-travel Scenario.sql
@@ -1,1 +1,9 @@
 -- This SQL will demonstrate how records change with time in a SQL table
+-- Note: This is not yet tested on SnowFlake Platform
+
+-- Delete a record
+DELETE FROM nyc_taxi_demo_db.serving_layer.final_taxi_data WHERE passenger_count = '2';
+
+-- Recover using Time Travel
+SELECT * FROM nyc_taxi_demo_db.serving_layer.final_taxi_data AT(OFFSET => -60);
+
